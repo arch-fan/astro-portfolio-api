7abf10646a554385e20415e8023dd3b2a500011d
arch-fan <juanserracines@gmail.com>
2023-11-05 11:18:55 +0100

    rename contact file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 // src/routes/index.ts
 
 import { type FastifyInstance } from 'fastify'
-import contactFormRoute from './contact-form'
+import contactFormRoute from './contact'
 
 const routes = async (fastify: FastifyInstance): Promise<void> => {
   void fastify.register(contactFormRoute, { prefix: '/contact' })
